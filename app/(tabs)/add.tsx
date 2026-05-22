38bf3889e37c1581033b0af3b4db911c30073335
Renato <renatofigmorais@gmail.com>
2026-05-22 00:20:18 -0400

    add fixes

--- app/(tabs)/add.tsx
+++ app/(tabs)/add.tsx
@@ -1,9 +1,22 @@
-import { View, Text } from 'react-native';
-
-export default function AddTransaction() {
-  return (
-    <View>
-      <Text>Adicionar Transação</Text>
-    </View>
-  );
+//teste tabs, futura tela de adicionar
+import { ScrollView, View, Text, StyleSheet } from 'react-native';
+import Header from '../../components/ui/header';
+import { SummaryCard } from '../../components/ui/summaryCard';
+import { Colors } from '../../constants/theme';
+export default function Add() {
+    return (
+        <ScrollView style={styles.container}> 
+            
+        </ScrollView>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: Colors.background,
+        paddingHorizontal: 5,
+        paddingTop: 0,
+        marginTop: 0,
+    },
+});
